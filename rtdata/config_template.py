7da2575bf1167ce854418bb124640176e6efcde8
Flavien Petit <flavien.petit@obs-vlfr.fr>
2025-04-07 10:25:57 +0100

    modify config file

--- rtdata/config_template.py
+++ rtdata/config_template.py
@@ -1,10 +1,25 @@
+#Mission details
+
+#Gliders
+gliders_units = ["unit_306","unit_352", "unit_408", "unit_436"]
+gliders_names = ["Zephyr", "OMG-1","Growler", "Stella"]
+gliders_ids = [127, 129, 55, 118]
+
+#Floats
+wmo = [6990668]
+
+#Credentials for API
 token = "your token in the prod env" 
 token_test = "your token in the test env"
 vpn_access = False #Changing this to True will lead to a request to the broker
+
+#Saving folders
 save_path = "path/to/save/files/"
+
 #broker
 broker = "seasystems.noc.soton.ac.uk"
 port = 1883
 topic = "topic/path"
 username = "usrname"
 password = "pwd"
+
